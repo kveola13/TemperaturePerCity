5bcdd577208d4040a98ae11d2400d19fa482f288
Ola Fredrik Ansnes Kveli <ola.fredrik.kveli@itera.com>
2023-01-30 08:09:22 +0100

    add tests

---
+++ TemperaturePerCityTests/Controllers/CityControllerTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TemperaturePerCity.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperaturePerCity.Controllers.Tests
+{
+    [TestClass()]
+    public class CityControllerTests
+    {
+        [TestMethod()]
+        public void CityControllerTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetCityDTOTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetCityDTOTest1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void PutCityDTOTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void PostCityDTOTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void DeleteCityDTOTest()
+        {
+            Assert.IsTrue(true); 
+        }
+
+        [TestMethod()]
+        public void InitTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetCitiesOrderTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetCitiesInReverseOrderTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetCitiesAlphabeticallyTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetCitiesByIdTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetCitiesByContinentTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetCitiesByTemperatureTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void GetCitiesByTimeTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
